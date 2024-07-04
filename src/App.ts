4cf9df53bcdece6681671b18ceac22fb4584c1dc
Protector1985 <michaelrbraun1985@gmail.com>
2024-07-04 06:32:17 -0700

    basic boilerplate

---
+++ src/App.ts
@@ -0,0 +1,22 @@
+import express, { Application } from "express";
+
+
+class App {
+    private app: Application
+    private port: number 
+    
+
+    constructor(props: {port:number}) {
+        this.app = express()
+        this.port = props.port
+    }
+
+
+    private startServer():void {
+        this.app.listen(this.port, () => {
+            console.log(`Server running on ${this.port}`)
+        })
+    }
+}
+
+export default App
